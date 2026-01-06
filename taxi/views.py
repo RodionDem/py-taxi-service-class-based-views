7094e889925589fa84cd8042554ff6c368342181
Rodion <r.demianetss@gmail.com>
2026-01-06 22:28:28 +0200

    Splution

--- taxi/views.py
+++ taxi/views.py
@@ -1,15 +1,68 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
-
-from taxi.models import Driver, Car, Manufacturer
+from django.views.generic import ListView, DetailView
+from .models import Driver, Car, Manufacturer
 
 
 def index(request):
-    """View function for the home page of the site."""
-
     context = {
         "num_drivers": Driver.objects.count(),
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
-
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    paginate_by = 5
+    queryset = Manufacturer.objects.all().order_by("name")
+    context_object_name = "manufacturer_list"
+
+
+class CarListView(ListView):
+    model = Car
+    paginate_by = 5
+    context_object_name = "car_list"
+
+    def get_queryset(self):
+        return Car.objects.select_related("manufacturer").all().order_by(
+            "manufacturer__name", "model"
+        )
+
+
+class CarDetailView(DetailView):
+    model = Car
+    context_object_name = "car"
+
+    def get_queryset(self):
+        return Car.objects.select_related("manufacturer")
+
+
+class DriverListView(ListView):
+    model = Driver
+    template_name = "taxi/driver_list.html"
+    context_object_name = "driver_list"
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Driver.objects.prefetch_related(
+            Prefetch(
+                "cars",
+                queryset=Car.objects.select_related("manufacturer")
+            )
+        )
+
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    context_object_name = "driver"
+
+    def get_queryset(self):
+        return Driver.objects.prefetch_related(
+            Prefetch(
+                "cars",
+                queryset=Car.objects.select_related("manufacturer")
+            )
+        )
